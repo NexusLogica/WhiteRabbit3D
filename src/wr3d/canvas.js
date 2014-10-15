189384a6be1976edc583e1b2a3982c36c2364289
Lawrence Gunn <lawrence.gunn@hightail.com>
2014-10-14 23:47:14 -0700

    Sprint 1: Working scripted mouse clicks onto canvas.

--- src/wr3d/canvas.js
+++ src/wr3d/canvas.js
@@ -26,18 +26,49 @@ Ngl.Canvas.prototype = {
     var _this = this;
     zebra()["zebra.json"] = "../src/lib/bower_components/zebra/zebra.json";
     zebra.ready(function() {
-      _this.texturemapWidth = _this.powerOfTwo(_this.width);
-      _this.texturemapHeight = _this.powerOfTwo(_this.height);
+
+      var json = '{'+
+        '  "ngl.width": 300,'+
+        '  "ngl.height": 200,'+
+        '  "padding": 4,'+
+        '  "layout": { "$zebra.layout.BorderLayout":[ 4, 4] },'+
+        '  "border": "plain",'+
+        '  "kids": {'+
+        '    "TOP": {'+
+        '      "$zebra.ui.BoldLabel": "Simple application",'+
+        '      "padding":4,'+
+        '      "background": "lightGray"'+
+        '    },'+
+        '    "CENTER": {'+
+        '      "$zebra.ui.TextArea": ""'+
+        '    },'+
+        '    "BOTTOM": {'+
+        '      "$zebra.ui.Button": "Clear"'+
+        '    }'+
+        '  }'+
+        '}';
+
+      var parsedJson = JSON.parse(json);
+
+      _this.texturemapWidth = _this.powerOfTwo(parsedJson['ngl.width']);
+      _this.texturemapHeight = _this.powerOfTwo(parsedJson['ngl.height']);
 
       _this.canvas = new zebra.ui.zCanvas(_this.canvasElement.get(0), _this.texturemapWidth, _this.texturemapHeight);
-
       _this.canvasElement.width(_this.texturemapWidth).height(_this.texturemapHeight);
 
-      _this.canvas.root.load(_this.layoutJsonUrl);
+      var bag = new zebra.util.Bag(_this.canvas.root);
+      bag.load(json);
 
       setTimeout(function() {
-        _this.canvasInitialized = true;
-      }, 1);
+        _this.canvas.root.setSize(parsedJson['ngl.width'], parsedJson['ngl.height']);
+        setTimeout(function() {
+          var button = _this.canvas.root.find('zebra.ui.Button');
+          button.bind(function() {
+            console.log("MOUSE");
+          });
+          _this.canvasInitialized = true;
+        }, 100);
+      }, 100);
     });
   },
 
@@ -60,3 +91,36 @@ Ngl.Canvas.prototype = {
     return this.canvasElement;
   }
 };
+
+function dispatchMouseDown(x, y) {
+  var edown = jQuery.Event( "mousedown", { pageX: x, pageY: y, button: 0 } );
+  $('.canvas-2d').trigger(edown);
+
+
+  setTimeout(function() {
+    var e = jQuery.Event( "mouseup", { pageX: x, pageY: y, button: 0 } );
+    $('.canvas-2d').trigger(e);
+
+    setTimeout(function() {
+      var ec = jQuery.Event( "click", { pageX: x, pageY: y, button: 0 } );
+      $('.canvas-2d').trigger(ec);
+    }, 5);
+  }, 5);
+}
+
+$('.simulate').click(function() {
+  dispatchMouseDown(145+window.pageXOffset, 180+window.pageYOffset);
+});
+
+$('.canvas-2d').on('click', function() {
+  console.log('CANVAS CLICKED');
+});
+
+$('.canvas-2d').on('mousedown', function() {
+  console.log('CANVAS MOUSEDOWN');
+});
+
+
+$('.canvas-2d').on('mouseup', function() {
+  console.log('CANVAS MOUSEUP');
+});
