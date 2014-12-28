0616dd69b4212a5d17e5627d795a479d67c2f346
Lawrence Gunn <lawrencegunn@gmail.com>
2014-12-27 19:51:31 -0800

    Working better with test transition for rotating elements.

--- src/wr3d/transition.js
+++ src/wr3d/transition.js
@@ -23,16 +23,25 @@ Ngl.Transition = function(scene, options) {
   this.options = options;
   this.startTime = scene.addTransition(this);
   this.endTime = this.startTime+this.options.duration;
+  this.ease = vec4.fromValues(0.250, 0.100, 0.250, 1.000);
 };
 
 Ngl.Transition.prototype.beforeRender = function(scene, time) {
   var last = (time >= this.endTime);
   var fraction = 1.0;
   if(!last) {
-    fraction = this.options.func((time-this.startTime)/this.options.duration);
+    fraction = Ngl.Transition.cubicSpline((time-this.startTime)/this.options.duration, 0.05, 0.95);
   }
   var value = fraction*(this.options.endValue-this.options.startValue)+this.options.startValue;
   var styleValue = this.options.styleString.replace('${0}', value);
   this.options.target.style(this.options.style, styleValue);
   return !last;
+};
+
+Ngl.Transition.cubicSpline = function(t, y1, y2) {
+  var cy = 3 * y1;
+  var by = 3 * (y2 - y1) - cy;
+  var ay = 1 - cy - by;
+
+  return t * (cy + t * (by + t * ay));
 };
